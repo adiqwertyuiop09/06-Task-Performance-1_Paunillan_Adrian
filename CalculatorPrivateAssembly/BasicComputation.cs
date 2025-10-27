fe16d31b283899efaf9b52923fef99a2c7b7b03a
adiqwertyuiop.09 <adiqwertyuiop.09@gmail.com>
2025-10-27 19:09:16 +0800

    created CalculatorPrivateAssembly and BasicComputation Class

--- CalculatorPrivateAssembly/BasicComputation.cs
+++ CalculatorPrivateAssembly/BasicComputation.cs
@@ -8,5 +8,32 @@ namespace CalculatorPrivateAssembly
 {
     public class BasicComputation
     {
+        float num1;
+        float num2;
+        float ans;
+
+        public float Addition(float num1, float num2, float ans)
+        { 
+            ans = ans + num1;
+            return ans;
+        }
+
+        public float Subtraction(float num1, float num2, float ans)
+        {
+            ans = ans - num1;
+            return ans;
+        }
+
+        public float Multiplication(float num1, float num2, float ans)
+        {
+            ans = ans * num1;
+            return ans;
+        }
+
+        public float Division(float num1, float num2, float ans)
+        {
+            ans = ans / num1;
+            return ans;
+        }
     }
 }
